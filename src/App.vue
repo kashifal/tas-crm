4c1ec6a5a25fead3a37ef5b77855756746895624
Kashif Sulehria <captainkashi4@gmail.com>
2023-04-09 12:25:54 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+ <div>
+<Header />  
+
+ <Transition>
+ <router-view></router-view>
+ </Transition>
+ </div>
+</template>
+
+<script setup>
+import Header from '@/components/Header.vue';
+
+</script>
+
+<style  scoped> 
+.v-enter-active,
+.v-leave-active {
+  transition: opacity .6s ease-in-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
